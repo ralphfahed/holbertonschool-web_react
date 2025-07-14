efda5b87444fdecead9e8ff34f3e4f0b1f49d6b9
ralph <ralphh.fahed@gmail.com>
2025-07-14 10:09:59 +0000

    4

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,13 +1,36 @@
-// Interface for the function
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
+// Interface for the constructor parameters
+interface StudentClassConstructor {
+  firstName: string;
+  lastName: string;
 }
 
-// Implementation of the function
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  return `${firstName.charAt(0)}. ${lastName}`;
-};
+// Interface describing the class instance (methods & properties)
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// Class implementing the interfaces
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
 
 // Example usage:
-console.log(printTeacher("John", "Doe")); // Output: J. Doe
+const student = new StudentClass("Ralph", "Fahed");
+console.log(student.displayName());    // Ralph
+console.log(student.workOnHomework()); // Currently working
 
