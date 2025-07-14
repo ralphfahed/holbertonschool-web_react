493311b255eae1c12a361c1340c3d0ad1c0cab1b
ralph <ralphh.fahed@gmail.com>
2025-07-14 10:20:26 +0000

    9

---
+++ TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,23 @@
+// task_4/js/subjects/Cpp.ts
+/// <reference path="Subject.ts" />
+/// <reference path="Teacher.ts" />
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingC?: number;
+  }
+
+  export class Cpp extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Cpp';
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingC > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+      return 'No available teacher';
+    }
+  }
+}
+
